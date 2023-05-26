75822f4ea797199dfc7318c7cb5fb39b08a6117c
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-05-26 19:47:02 +0300

    initial commit

---
+++ src/containers/index.ts
@@ -0,0 +1,23 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import About from './About';
+import Social from './Social';
+import Email from './Email';
+import Contact from './Contact';
+import Footer from './Footer';
+import SocialLinks from './SocialLinks';
+import Projects from './Projects';
+import Layout from './Layout';
+
+export {
+  Navbar,
+  Hero,
+  About,
+  Social,
+  Email,
+  Contact,
+  Footer,
+  SocialLinks,
+  Projects,
+  Layout,
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from 'next';
+import {
+  About,
+  Hero,
+  Contact,
+  Projects,
+  Layout,
+} from '../containers';
+
+/**
+ * TODO: Create separate page for all the projects with filters (vercel | netlify | github api for automation)
+ * TODO: Switch to next13 app dir feature, when lottie files start working in app dir
+ * TODO: Try test cases
+ */
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Layout>
+        <Hero />
+        <About />
+        <Contact />
+      </Layout>
+    </>
+  );
+};
+
+export default Home;
